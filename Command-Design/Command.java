82424d0a4940eaee584af45945a6ef15c071ac31
jordan-wood02 <111987466+jordan-wood02@users.noreply.github.com>
2023-02-26 20:57:04 -0500

    Updating classes with Javadoc

--- Command-Design/Command.java
+++ Command-Design/Command.java
@@ -1,3 +1,13 @@
+package command;
+
+/**
+ * Represents a command that can be executed
+ * Classes that implement the Command interface must
+ * implement the execute method
+ */
 public interface Command {
+    /**
+     * Executes the command
+     */
     public void execute();
 }
--- Command-Design/InputHandler.java
+++ Command-Design/InputHandler.java
@@ -1,9 +1,22 @@
+package command;
+
 import java.util.HashMap; // import the HashMap class
 
+/**
+ * Responsible for handling user input and executing
+ * the appropriate command
+ */
 public class InputHandler {
 
     private HashMap<String, Command> commands = new HashMap<String, Command>();
 
+    /**
+     * Constructs new FiveMinCommand object with stopwatch
+     * Creates new instances of the ThirtySecCommand, OneMinCommand, and
+     * FiveMinCommand classes and adds them to the commands HashMap
+     * 
+     * @param watch the stopwatch
+     */
     public InputHandler(StopWatch watch) {
         ThirtySecCommand thirtyCom = new ThirtySecCommand(watch);
         OneMinCommand oneCom = new OneMinCommand(watch);
@@ -13,6 +26,13 @@ public class InputHandler {
         commands.put("5", fiveCom);
     }
 
+    /**
+     * Checks if the given input string corresponds to a valid command
+     * in the commands HashMap.
+     * 
+     * @param data the input string
+     * @return true if input corresponds to command, false otherwise
+     */
     public boolean inputEntered(String data) {
         if (commands.containsKey(data)) {
             return true;
